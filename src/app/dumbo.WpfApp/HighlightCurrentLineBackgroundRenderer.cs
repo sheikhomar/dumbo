98ab28fb01be590386366a95f8bcaf99e29caa9a
Omar Ali Sheikh-Omar <sheikhomar@gmail.com>
2016-03-13 20:22:27 +0100

    Highlight current line in the editor.

---
+++ src/app/dumbo.WpfApp/HighlightCurrentLineBackgroundRenderer.cs
@@ -0,0 +1,41 @@
+using System.Windows;
+using System.Windows.Media;
+using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Rendering;
+
+namespace dumbo.WpfApp
+{
+    // Stolen from http://stackoverflow.com/questions/5072761/avalonedit-highlight-current-line-even-when-not-focused
+    public class HighlightCurrentLineBackgroundRenderer : IBackgroundRenderer
+    {
+        private readonly TextEditor _editor;
+
+        public HighlightCurrentLineBackgroundRenderer(TextEditor editor)
+        {
+            _editor = editor;
+            _editor.TextArea.Caret.PositionChanged += (sender, e) =>
+                _editor.TextArea.TextView.InvalidateLayer(KnownLayer.Background);
+        }
+
+        public KnownLayer Layer
+        {
+            get { return KnownLayer.Caret; }
+        }
+
+        public void Draw(TextView textView, DrawingContext drawingContext)
+        {
+            if (_editor.Document == null)
+                return;
+
+            textView.EnsureVisualLines();
+            var currentLine = _editor.Document.GetLineByOffset(_editor.CaretOffset);
+            foreach (var rect in BackgroundGeometryBuilder.GetRectsForSegment(textView, currentLine))
+            {
+                var rect2 = new Rect(new Point(rect.Location.X + textView.ScrollOffset.X, rect.Location.Y), new Size(textView.ActualWidth, rect.Height));
+                drawingContext.DrawRectangle(
+                    new SolidColorBrush(Color.FromArgb(0x10, 0, 0, 0xFF)), null,
+                    rect2);
+            }
+        }
+    }
+}
--- src/app/dumbo.WpfApp/MainWindow.xaml.cs
+++ src/app/dumbo.WpfApp/MainWindow.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ICSharpCode.AvalonEdit.Highlighting;
 using Microsoft.Win32;
 using Path = System.IO.Path;
@@ -45,6 +35,10 @@ namespace dumbo.WpfApp
             textEditor.TextArea.Caret.PositionChanged += CaretOnPositionChanged;
 
             textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinition("VB");
+            textEditor.TextArea.TextView.BackgroundRenderers.Add(
+                new HighlightCurrentLineBackgroundRenderer(textEditor));
+
+            textEditor.Focus();
 
             UpdateCaretPosition();
             LoadDefaultProgram();

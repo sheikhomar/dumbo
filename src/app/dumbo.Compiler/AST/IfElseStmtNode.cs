deb41bd9c32a90b384ed0231d358b865ea845c45
Omar Ali Sheikh-Omar <sheikhomar@gmail.com>
2016-03-28 16:04:40 +0200

    Remove duplicate code.

--- src/app/dumbo.Compiler/AST/IfElseStmtNode.cs
+++ src/app/dumbo.Compiler/AST/IfElseStmtNode.cs
@@ -37,12 +37,7 @@ namespace dumbo.Compiler.AST
 
         public override void CCAnalyse(ICCAnalyser analyser)
         {
-            //Check predicate
-            if (Predicate.GetHappyType(analyser.SymbolTable).GetFirst() != HappyType.Boolean)
-                analyser.ErrorReporter.AddError("IfElse Statement did not have a predicate of type Boolean");
-
-            //Check body
-            Body.CCAnalyse(analyser);
+            base.CCAnalyse(analyser);
             Else.CCAnalyse(analyser);
         }
     }

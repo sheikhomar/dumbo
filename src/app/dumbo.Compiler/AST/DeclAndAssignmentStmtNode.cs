5528c5269290ac13affd81feb0f1d8b0c4457ca2
MarcBeuchert <marcbeuchert@hotmail.com>
2016-03-29 10:47:55 +0200

    Changed the CCAnalyse for decl and assign stmt

--- src/app/dumbo.Compiler/AST/DeclAndAssignmentStmtNode.cs
+++ src/app/dumbo.Compiler/AST/DeclAndAssignmentStmtNode.cs
@@ -30,30 +30,58 @@ namespace dumbo.Compiler.AST
 
         public override void CCAnalyse(ICCAnalyser analyser)
         {
-            //Check all Type and literal, remember that a, b = 5 ie last exp
-            //This must be done before scope stuff.
-
-            // Generating TypeLists
-            var idTypeList = GetHappyTypeList(Identifiers, analyser.SymbolTable);
-            var exprTypeList = GetHappyTypeList(Expressions, analyser.SymbolTable);
-
-            // Checking for correct id and expr count
-            if (exprTypeList.Count < exprTypeList.Count)
-                analyser.ErrorReporter.AddError("Decl Ass Error: Too many expressions compared to identifiers");
-            else if (idTypeList.Count > exprTypeList.Count && exprTypeList.Count != 1)
-                analyser.ErrorReporter.AddError("Decl Ass Error: Incompatible amount of identifiers and expressions.");
-
-            // Checking for correct types in expr compared to the declaration type
-            if (exprTypeList.Count == 1 && !analyser.IsEqual(exprTypeList[0], Type))
-                analyser.ErrorReporter.AddError("Type Error: The assignment is not type correct.");
-            else if (!analyser.IsListEqualToType(exprTypeList, Type))
-                analyser.ErrorReporter.AddError("Type Error: The assignment is not type correct.");
-
-            // Adding identifiers to symboltable
-            foreach (var id in Identifiers)
+            // Check for the correct count of identifiers and expressions
+            if (Identifiers.Count < Expressions.Count)
             {
-                analyser.AddVariableToSymbolTable(id.Name, Type, false, Line, Column);
+                analyser.ErrorReporter.AddError("Assignment Error: More Expressions than identifiers");
+                return;
             }
+
+            IList<HappyType> exprTypeList = CheckExpressionForMultipleReturnTypes(analyser);
+
+            if (Identifiers.Count > exprTypeList.Count)
+                analyser.ErrorReporter.AddError("Assignment Error: Too many identifiers compared to expression types.");
+            else if (Identifiers.Count < exprTypeList.Count)
+                analyser.ErrorReporter.AddError("Assignment Error: Too many expression types compared to identifiers.");
+
+            if (Identifiers.Count == exprTypeList.Count)
+            {
+                for (int i = 0; i < Identifiers.Count; i++)
+                {
+                    var id = Identifiers[i];
+                    var exprType = exprTypeList[i];
+                    if (Type == exprType)
+                        analyser.AddVariableToSymbolTable(id.Name, Type, false, Line, Column);
+                    else
+                        analyser.ErrorReporter.AddError(new CCError($"Identifier '{id.Name}' has a different type than the assigned expression", Line, Column));
+                }
+            }
+
+            ////================================    OLD    ====================================//
+            ////Check all Type and literal, remember that a, b = 5 ie last exp
+            ////This must be done before scope stuff.
+
+            //// Generating TypeLists
+            //var idTypeList = GetHappyTypeList(Identifiers, analyser.SymbolTable);
+            //var exprTypeList = GetHappyTypeList(Expressions, analyser.SymbolTable);
+
+            //// Checking for correct id and expr count
+            //if (exprTypeList.Count < exprTypeList.Count)
+            //    analyser.ErrorReporter.AddError("Decl Ass Error: Too many expressions compared to identifiers");
+            //else if (idTypeList.Count > exprTypeList.Count && exprTypeList.Count != 1)
+            //    analyser.ErrorReporter.AddError("Decl Ass Error: Incompatible amount of identifiers and expressions.");
+
+            //// Checking for correct types in expr compared to the declaration type
+            //if (exprTypeList.Count == 1 && !analyser.IsEqual(exprTypeList[0], Type))
+            //    analyser.ErrorReporter.AddError("Type Error: The assignment is not type correct.");
+            //else if (!analyser.IsListEqualToType(exprTypeList, Type))
+            //    analyser.ErrorReporter.AddError("Type Error: The assignment is not type correct.");
+
+            //// Adding identifiers to symboltable
+            //foreach (var id in Identifiers)
+            //{
+            //    analyser.AddVariableToSymbolTable(id.Name, Type, false, Line, Column);
+            //}
         }
 
         private IList<HappyType> GetHappyTypeList(IEnumerable<ExpressionNode> nodeList, ISymbolTable st)

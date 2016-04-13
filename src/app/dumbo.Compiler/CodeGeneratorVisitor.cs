06cbd57a57618be8bbdfc6801263207f4ae6eb60
Omar Ali Sheikh-Omar <sheikhomar@gmail.com>
2016-04-13 15:38:00 +0200

    Begin code generator.

---
+++ src/app/dumbo.Compiler/CodeGeneratorVisitor.cs
@@ -0,0 +1,146 @@
+using dumbo.Compiler.AST;
+
+namespace dumbo.Compiler
+{
+    public class RuntimeEntity
+    {
+    }
+
+    public class CodeGeneratorVisitor : IVisitor<RuntimeEntity, VisitorArgs>
+    {
+        public RuntimeEntity Visit(ActualParamListNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(AssignmentStmtNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(BinaryOperationNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(BreakStmtNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(DeclAndAssignmentStmtNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(DeclStmtNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(ElseIfStmtListNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(ElseIfStmtNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(ExpressionListNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(FormalParamListNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(FormalParamNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(FuncCallExprNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(FuncCallStmtNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(FuncDeclListNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(FuncDeclNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(IdentifierListNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(IdentifierNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(IfElseStmtNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(IfStmtNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(LiteralValueNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(ProgramNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(RepeatStmtNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(RepeatWhileStmtNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(ReturnStmtNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(RootNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(StmtBlockNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RuntimeEntity Visit(UnaryOperationNode node, VisitorArgs arg)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

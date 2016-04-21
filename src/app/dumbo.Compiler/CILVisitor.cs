b070f42b5a17a1123280936ce9398334139524d8
MarcBeuchert <marcbeuchert@hotmail.com>
2016-04-21 10:29:56 +0200

    added a visitor for CIL

---
+++ src/app/dumbo.Compiler/CILVisitor.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dumbo.Compiler.AST;
+
+namespace dumbo.Compiler
+{
+    class CommonIntermediateLanguageVisitor : IVisitor<VisitResult, VisitorArgs>
+    {
+        public VisitResult Visit(ActualParamListNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(AssignmentStmtNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(BinaryOperationNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(BreakStmtNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(BuiltInFuncDeclNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(DeclAndAssignmentStmtNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(DeclStmtNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(ElseIfStmtListNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(ElseIfStmtNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(ExpressionListNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(FormalParamListNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(FormalParamNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(FuncCallExprNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(FuncCallStmtNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(FuncDeclListNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(FuncDeclNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(IdentifierListNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(IdentifierNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(IfElseStmtNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(IfStmtNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(LiteralValueNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(ProgramNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(RepeatStmtNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(RepeatWhileStmtNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(ReturnStmtNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(RootNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(StmtBlockNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VisitResult Visit(UnaryOperationNode node, VisitorArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

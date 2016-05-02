7f646ca9396d74768aea4365f51d989f21186621
stelle <stellehj@yahoo.dk>
2016-05-02 10:46:46 +0200

    Fixed Break error :potable_water:

--- src/app/dumbo.Compiler/SyntaxAnalysis/AbstractSyntaxTreeBuilder.cs
+++ src/app/dumbo.Compiler/SyntaxAnalysis/AbstractSyntaxTreeBuilder.cs
@@ -127,8 +127,8 @@ namespace dumbo.Compiler.SyntaxAnalysis
             Debug.Assert(token.Parent.Name() == "BreakStmt");
             Reduction rhs = (Reduction)token.Data;
 
-            Reduction rhs2 = (Reduction)rhs[0].Data;
-            var srcPos = BuildSourcePosition(rhs2[0], rhs2[0]);
+            //Reduction rhs2 = (Reduction)rhs[0].Data;
+            var srcPos = BuildSourcePosition(rhs[0], rhs[0]);
             return new BreakStmtNode(srcPos);
         }
 

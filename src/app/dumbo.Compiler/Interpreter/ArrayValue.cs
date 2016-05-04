cb35ca784fbf3a06a265f75023a1406cdc190f3a
MarcBeuchert <marcbeuchert@hotmail.com>
2016-05-04 14:20:04 +0200

    Added a ArrayValue for interpretation

---
+++ src/app/dumbo.Compiler/Interpreter/ArrayValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dumbo.Compiler.AST;
+
+namespace dumbo.Compiler.Interpreter
+{
+    class ArrayValue : Value
+    {
+        public TypeNode Type { get; }
+        private Value[] InternalArray;
+
+        public ArrayValue(TypeNode type, ExpressionListNode sizes)
+        {
+            Type = type;
+        }
+    }
+}

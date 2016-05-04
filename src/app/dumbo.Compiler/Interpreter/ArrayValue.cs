4b763cd946748fb7ac81d5a7c19bcc615ba42e3e
MarcBeuchert <marcbeuchert@hotmail.com>
2016-05-04 16:00:54 +0200

    Added get and set value to ArrayValue

--- src/app/dumbo.Compiler/Interpreter/ArrayValue.cs
+++ src/app/dumbo.Compiler/Interpreter/ArrayValue.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using dumbo.Compiler.AST;
@@ -9,12 +10,90 @@ namespace dumbo.Compiler.Interpreter
 {
     class ArrayValue : Value
     {
-        public TypeNode Type { get; }
-        private Value[] InternalArray;
+        public PrimitiveType Type { get; }
+        public List<int> Sizes { get; }
+        private List<Value> InternalArray { get; }
+        public Value this[int i]
+        {
+            get { return InternalArray[i]; }
+            set { InternalArray[i] = value; }
+        }
 
-        public ArrayValue(TypeNode type, ExpressionListNode sizes)
+
+        public ArrayValue(PrimitiveType type, List<int> sizes)
         {
             Type = type;
+            Sizes = sizes;
+            InternalArray = new List<Value>();
+            InitializeArray();
+        }
+
+        private void InitializeArray()
+        {
+            if (Sizes.Count == 1)
+            {
+                for (int i = 0; i < Sizes[0]; i++)
+                {
+                    switch (Type)
+                    {
+                        case PrimitiveType.Number:
+                            InternalArray.Add(new NumberValue(0));
+                            break;
+                        case PrimitiveType.Text:
+                            InternalArray.Add(new TextValue(""));
+                            break;
+                        case PrimitiveType.Boolean:
+                            InternalArray.Add(new BooleanValue(false));
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < Sizes[0]; i++)
+                {
+                    InternalArray.Add(new ArrayValue(Type, Sizes.Skip(1).ToList()));
+                }
+            }
+        }
+
+        public Value GetValue(List<int> indices)
+        {
+            return GetValue(this, indices);
+        }
+
+        private Value GetValue(ArrayValue array, List<int> indices)
+        {
+            if (indices.Count == 1)
+            {
+                return array[indices[0]];
+            }
+            else
+            {
+                var newArray = array[indices[0]] as ArrayValue;
+                var value = GetValue(newArray, indices.Skip(1).ToList());
+                return value;
+            }
+        }
+
+        public void SetValue(List<int> indices, Value value)
+        {
+            SetValue(this, indices, value);
+        }
+
+        private void SetValue(ArrayValue array, List<int> indices, Value value)
+        {
+            if (indices.Count == 1)
+            {
+                array[indices[0]] = value;
+            }
+            else
+            {
+                var newArray = array[indices[0]] as ArrayValue;
+                SetValue(newArray, indices.Skip(1).ToList(), value);
+            }
         }
     }
 }

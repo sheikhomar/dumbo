fdf60f155d0afffe6d99022fb0f806a772aacc23
stelle <stellehj@yahoo.dk>
2016-05-20 13:34:25 +0200

    Saved settings for running the program in window mode :sagittarius:

--- src/app/dumbo.Compiler/CProgramRunner.cs
+++ src/app/dumbo.Compiler/CProgramRunner.cs
@@ -30,5 +30,30 @@ namespace dumbo.Compiler
 
             return result;
         }
+
+        // Without running the program inside ILE but in a separate windows
+        //public ProcessResult Run(string binaryPath)
+        //{
+        //    var targetFile = new FileInfo(binaryPath);
+        //    var result = new ProcessResult();
+        //
+        //    Process process = new Process();
+        //    process.StartInfo.CreateNoWindow = false;
+        //    process.StartInfo.WorkingDirectory = Environment.CurrentDirectory;
+        //    process.StartInfo.UseShellExecute = true;
+        //    process.StartInfo.RedirectStandardError = false;
+        //    process.StartInfo.RedirectStandardOutput = false;
+        //    process.StartInfo.FileName = targetFile.FullName;
+        //    //process.OutputDataReceived += (s, e) => result.AddOutput(e.Data);
+        //    //process.ErrorDataReceived += (s, e) => result.AddError(e.Data);
+        //    process.Start();
+        //    //process.BeginOutputReadLine();
+        //    //process.BeginErrorReadLine();
+        //    process.WaitForExit();
+        //    result.StatusCode = process.ExitCode;
+        //    process.Close();
+        //
+        //    return result;
+        //}
     }
 }
